cdf6708b657306da4ac1d6a67cc7ff4c4b70f5f8
Puneet <puneetmakhija4@gmail.com>
2021-04-27 18:56:44 +0530

    aaaa

--- pv-rebalance/celery.py
+++ pv-rebalance/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_portal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pv-rebalance.settings')
 
-app = Celery('music_portal', broker='redis://redis:6379/0')
+app = Celery('pv-rebalance', broker='redis://redis:6379/0')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
