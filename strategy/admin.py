0f78c8ca8904a490320e61016fae5e7d51fefcb2
Puneet <puneetmakhija4@gmail.com>
2021-04-29 23:39:01 +0530

    rc

--- strategy/admin.py
+++ strategy/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Strategy
 # Register your models here.
 class StrategyAdmin(admin.ModelAdmin):
-	list_display = ('name', 'source', 'funds', 'created')
-
+	list_display = ('name', 'source', 'get_funds', 'created')
+	def get_funds(self, obj):
+		return str(obj.funds) + '%'
 
 
 
