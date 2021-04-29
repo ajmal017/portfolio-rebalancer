d4a0371f8fe1256aadc8ec439ff935f7ed8fc147
Puneet <puneetmakhija4@gmail.com>
2021-04-29 23:58:55 +0530

    asdsda

--- strategy/models.py
+++ strategy/models.py
@@ -6,9 +6,20 @@ class Strategy(models.Model):
 	class Meta:
 		verbose_name = "Strategy"
 		verbose_name_plural = "Strategies"
+
+	PORTFOLIO_VISUALIZER = 'PORTFOLIO VISUALIZER'
+	VTS = 'VTS'
+	TRENDSEOEXPLORER = 'TRENDSEOEXPLORER'
 	
+
+	TYPES = (
+		(PORTFOLIO_VISUALIZER, ('Portfolio visualizer')), 
+		(VTS, ('Vts')), 
+		(TRENDSEOEXPLORER, ('TRENDSEOEXPLORER'))
+	)
 	name = models.CharField(verbose_name="Strategy Name", max_length=300)
-	source = models.CharField(verbose_name="Data Source", max_length=300)
+	# source = models.CharField(verbose_name="Data Source", max_length=300)
+	source =  models.CharField(verbose_name=('Data Source'), max_length=100, blank=False, choices=TYPES, null=True)
 	funds = models.CharField(verbose_name="Allocated Funds", max_length=300)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
