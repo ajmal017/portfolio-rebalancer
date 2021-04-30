2ed5cdaf07b25170e870b3bdea299c0a436b5c41
Puneet <puneetmakhija4@gmail.com>
2021-04-30 23:31:58 +0530

    rc

---
+++ scraping_events/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'scraping_events.apps.ScrapingEventsConfig'
--- scraping_events/apps.py
+++ scraping_events/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ScrapingEventsConfig(AppConfig):
     name = 'scraping_events'
+    verbose_name = "Scraping Events"
