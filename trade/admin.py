7673619e1a423c1e9c3899684279861ff71e3f4b
Puneet <puneetmakhija4@gmail.com>
2021-05-03 16:47:54 +0530

    rs

--- trade/admin.py
+++ trade/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from .models import Trade
 # Register your models here.
 class TradeAdmin(admin.ModelAdmin):
-	list_display = ('get_strategy', 'trade_fund', 'get_trading_account', 'timestamp')
-
-	def get_strategy(self, obj):
+	def strategy(self, obj):
 		return str(obj.strategy.name)
 
-	def get_trading_account(self, obj):
+	def trading_account(self, obj):
 		return str(obj.trading_account.platform_name)
+
+	list_display = ('strategy', 'trade_fund', 'trading_account', 'timestamp')
+
+	
 
 
 
