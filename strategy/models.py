bb6d64acc597b81ecc44420b7ee3bd5c46f584b8
Puneet <puneetmakhija4@gmail.com>
2021-05-07 22:59:34 +0530

    add

--- strategy/models.py
+++ strategy/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Strategy(models.Model):
@@ -17,8 +18,15 @@ class Strategy(models.Model):
 		(VTS, ('VTS')), 
 		(TRENDXPLRER, ('TrendXpLrer'))
 	)
+	SCRAPEFREQUENCY = (
+        ('Daily','Daily'),
+        ('Weekly','Weekly'),
+        ('Monthly','Monthly')
+    )
 	name = models.CharField(verbose_name="Strategy Name", max_length=300)
 	# source = models.CharField(verbose_name="Data Source", max_length=300)
+	scrape_frequency = models.CharField(verbose_name=('Scrape Frequency'), max_length=50, default='Daily', choices=SCRAPEFREQUENCY)
+	is_active = models.BooleanField(default=False)
 	display_name = models.CharField(verbose_name="Display Name", max_length=200, blank=False, null=True)
 	source =  models.CharField(verbose_name=('Data Source'), max_length=100, blank=False, choices=TYPES, null=True)
 	funds = models.CharField(verbose_name="Allocated Funds", max_length=300)
@@ -27,3 +35,9 @@ class Strategy(models.Model):
 
 	def __str__(self):
 		return self.name
+
+	def clean(self, *args, **kwargs):
+		name = str(self.name)
+		if name and Strategy.objects.filter(name=name).exists():
+			raise ValidationError('Strategy name already exists')
+		super(Strategy, self).clean(*args, **kwargs)
