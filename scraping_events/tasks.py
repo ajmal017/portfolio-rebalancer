6757f557ba8d221881c28bf9ca746e4b2febedfc
Puneet <puneetmakhija4@gmail.com>
2021-06-09 20:02:06 +0530

    a

--- scraping_events/tasks.py
+++ scraping_events/tasks.py
@@ -6,7 +6,7 @@ def send_report_email(message):
 	subject = 'Portfolio Visualizer'
 	from_email = settings.MAIL_FROM
 	try:
-		message = EmailMessage(subject, message, from_email, 'er.mohittambi@gmail.com')
+		message = EmailMessage(subject, message, from_email, ['er.mohittambi@gmail.com'])
 		message.send()
 	except Exception as ex:
 		print("Failed to send", ex)
