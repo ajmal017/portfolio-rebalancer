db84e3f03ecff11dcabdf3ed9e00904d3f340267
Puneet <puneetmakhija4@gmail.com>
2021-06-09 19:37:40 +0530

    n

--- scraping_events/tasks.py
+++ scraping_events/tasks.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.mail import EmailMessage
 
 
-def send_report_email(, message):
+def send_report_email(message):
 	subject = 'Portfolio Visualizer'
 	from_email = settings.MAIL_FROM
 	try:
